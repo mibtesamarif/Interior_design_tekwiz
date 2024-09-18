aabd378d376aaafb6adbc5b335d45da26e725c6e
mibtesamarif <m.ibtesam.arif17@gmail.com>
2024-09-18 11:03:50 +0500

    first Commit

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2024, 11, 01, 8)}); // year, month, date, hour
+        });
+});		
+
